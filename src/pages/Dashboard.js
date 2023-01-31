490f8d17c7cee5219efaf6d93109add2c409ce2e
Saloni <salonishah306@gmail.com>
2023-01-31 17:37:10 +0530

    Not full dashboard

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div className=" bg-lightBlue   ">
+            <div className="text-black font-bold text-3xl  ">Student's Dashboard</div>
+            <div className="text-black font-bold text-3xl  ">Academic Performance</div>
+            
+            <div className="flex ">
+                <div className="bg-white w-2/5 h-40 rounded-md  mr-10"></div>
+                <div className="bg-white w-72 h-40 rounded-md mr-10 ml-10 "></div>
+            </div>
+            <div className=""></div>
+
+
+
+        </div>
+
+
+
+    )
+}
+
+export default Dashboard;
