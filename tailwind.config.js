24c7b8b5a4e6c09be8596059b6c34b4cf3443401
Saloni <salonishah306@gmail.com>
2023-01-31 17:33:40 +0530

    colours added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  theme: {  
+    extend: {
+      colors: {
+      blue:"#3b54f3",
+      background :"#D4D9F6",
+      lightBlue: "#D6E8FB",
+      },
+      fontFamily: {
+        main: ["IBM Plex Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
+  
 }
