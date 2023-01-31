25f80f56809a73880bd17e2e63da6c6f38cc1ec0
Saloni <salonishah306@gmail.com>
2023-01-31 17:33:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
 import React from "react";
+
+import SideBar from './components/SideBarStud';
+import Dashboard  from './pages/Dashboard';
+import SideBarTeach from './components/SideBarTeach';
+import NavBar from './components/NavBar';
+import  DashBoardCard  from './components/DashBoardCard';
+import DashBoardCardTeach from './components/DasBoardCardTeach';
+import Risk from './pages/Risk';
+import FormStud from './pages/FormStud';
+import FormTeach from './pages/FormTeach';
+import Chart from './components/Chart';
+import DropDown from './components/DropDown';
+
+
 function App() {
   return (
    
-      <div className="App font-main h-full overflow-x-hidden"></div>
+      <div className="App">
+        
+         {/* <SideBar /> */}
+         {/* <Dashboard/> */}
+         {/* <SideBarTeach/> */}
+         {/* <NavBar/> */}
+         {/* <DashBoardCard/> */}
+         {/* <DashBoardCardTeach/> */}
+         {/* <Risk/> */}
+         {/* <FormStud/> */}
+         {/* <FormTeach/> */}
+         {/* <Chart/> */}
+         <DropDown />
+         
+      </div>
     
   );
 }
